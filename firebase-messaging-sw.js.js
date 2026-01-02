62c2f97607f158eccf2493ee919c75d150c6e55a
MD. HUMAUN KOBIR SHORIF <hosain91910@gmail.com>
2026-01-02 15:25:52 +0600

    Replace Firebase config placeholders with values
    Updated Firebase configuration with actual credentials.

--- firebase-messaging-sw.js.js
+++ firebase-messaging-sw.js.js
@@ -3,10 +3,10 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js')
 
 // আপনার Firebase Config এখানে পুনরায় দিন
 firebase.initializeApp({
-    apiKey: "YOUR_API_KEY",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID",
-    projectId: "YOUR_PROJECT_ID"
+    apiKey: "AIzaSyDCKr5EaLVrDkV4TG00ortCiTWIAo5zgjc",
+    messagingSenderId: "258135136638",
+    appId: "1:258135136638:web:2f0e0086213343177bbb2f",
+    projectId: "vimo-d453c",
 });
 
 const messaging = firebase.messaging();
